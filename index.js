82ee647a67dfb78428259db7ee1572432e9b835c
Mijan Karim <mkatbd@gmail.com>
2021-04-18 01:49:48 +0600

    added more endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5050;
@@ -17,6 +17,92 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const servicesCollection = client.db("cityElectric").collection("services");
+  const reviewsCollection = client.db("cityElectric").collection("reviews");
+  const ordersCollection = client.db("cityElectric").collection("orders");
+  const adminsCollection = client.db("cityElectric").collection("admins");
+
+  app.get("/services", (req, res) => {
+    servicesCollection.find().toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/service/:id", (req, res) => {
+    servicesCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  app.post("/addService", (req, res) => {
+    const newService = req.body;
+    servicesCollection.insertOne(newService).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/addReview", (req, res) => {
+    const newReview = req.body;
+    reviewsCollection.insertOne(newReview).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find().toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/addAdmin", (req, res) => {
+    const newAdmin = req.body;
+    adminsCollection.insertOne(newAdmin).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/isAdmin", (req, res) => {
+    const email = req.body.email;
+    adminsCollection.find({ email: email }).toArray((err, admins) => {
+      res.send(admins.length > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    ordersCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.get("/ordersList", (req, res) => {
+    ordersCollection
+      .find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.delete("/delete/:id", (req, res) => {
+    servicesCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
 });
 
 app.listen(port);
